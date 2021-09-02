6591570d682113dab7489a50c74b4202ade906f5
Tajuddin-Ahmed <tajuddincse13@gmail.com>
2021-09-02 14:41:00 +0600

    finally completed

--- js/book.js
+++ js/book.js
@@ -1,23 +1,26 @@
 const loadSearch = async () => {
+    
     document.getElementById('searchResult').classList.add('d-none');
+    // spinner 
     document.getElementById('spinner').classList.add('spinner-border');
+    // Fetching data from api 
     const searchContent = document.getElementById('searchedBook');
     const searchValue = searchContent.value;
     searchContent.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchValue}`;
+    const url = `https://openlibrary.org/search.json?q=${searchValue}`;
     const res = await fetch(url);
     const data = await res.json();
-    showResults(data.docs,data.numFound);
+    showResults(data.docs.slice(0, 30), data.numFound);
 }
 
-const showResults = (books,numFound) => {
-    console.log(books);  
+const showResults = (books, numFound) => {
+    // showing no result 
     if ((Object.keys(books).length) === 0) {
         document.getElementById('noData').innerText = "No Data Found";
         document.getElementById('showResult').textContent = '';
         document.getElementById('dataNum').innerText = '';
     } else {
-        document.getElementById('noData').innerText='';
+        document.getElementById('noData').innerText = '';
         const container = document.getElementById('showResult');
         document.getElementById('dataNum').innerText = `Showing ${Object.keys(books).length} From Total ${numFound} Results `;
         container.textContent = '';
@@ -35,6 +38,7 @@ const showResults = (books,numFound) => {
             container.appendChild(div);
         });
     }
+    // spinner off when results have been shown 
     document.getElementById('spinner').classList.remove('spinner-border');
     document.getElementById('searchResult').classList.remove('d-none');
 }
